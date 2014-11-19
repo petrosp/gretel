1777b66acdb16cda2d5385577190d47a6df6ca04
Petros Papadopoulos <petros.papadopoulos@ams-ix.net>
2014-11-19 18:22:15 +0100

    Instance Vars are now available

--- lib/gretel/renderer.rb
+++ lib/gretel/renderer.rb
@@ -54,10 +54,12 @@ module Gretel
         yield parent
       end
     end
+    
+    attr_reader :breadcrumb_key, :breadcrumb_args
 
     private
 
-    attr_reader :context, :breadcrumb_key, :breadcrumb_args
+    attr_reader :context
 
     # Returns merged options for rendering breadcrumbs.
     def options_for_render(options = {})
